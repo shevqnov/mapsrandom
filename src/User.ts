a7a19d92d16734abcc6e3c753a9312858feddb83
shevqnov <sshevqnov@yandex.ru>
2019-07-23 15:05:43 +0500

    oh, shit

---
+++ src/User.ts
@@ -0,0 +1,16 @@
+import faker from "faker";
+
+export class User {
+  name: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+  constructor() {
+    this.name = `${faker.name.firstName()} ${faker.name.lastName()}`;
+    this.location = {
+      lat: Number(faker.address.latitude()),
+      lng: Number(faker.address.longitude())
+    };
+  }
+}
