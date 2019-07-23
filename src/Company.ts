b247ae72a3845fd207f6c79118451a188978683f
shevqnov <sshevqnov@yandex.ru>
2019-07-23 16:23:01 +0500

    defaning company

---
+++ src/Company.ts
@@ -0,0 +1,17 @@
+import faker from "faker";
+export class Company {
+  name: string;
+  catchPhrase: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+  constructor() {
+    this.name = faker.company.companyName();
+    this.catchPhrase = faker.company.catchPhrase();
+    this.location = {
+      lat: Number(faker.address.latitude()),
+      lng: Number(faker.address.longitude())
+    };
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('vroom vroom')
+import { User } from "./User";
+import { Company } from "./Company";
+
+const user = new User();
+const company = new Company();
+
+console.log("user", { name: user.name, location: user.location });
+console.log("company", {
+  name: company.name,
+  location: company.location,
+  cp: company.catchPhrase
+});
